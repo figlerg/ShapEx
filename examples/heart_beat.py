745fb421054cf7eb338aa7a3b3973ed51d4c5390
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2021-07-16 18:18:09 +0200

    Added the skeleton of the heart beat example.

---
+++ examples/heart_beat.py
@@ -0,0 +1,25 @@
+from shapex.shapex.ShapEx import *
+import os
+
+# this is just to get the path to the current working directory.
+#  In a normal usecase you would instead just need the path to the specification file
+cwd = os.getcwd()
+
+path = os.path.join(cwd, 'heart_beat.se')
+
+# initialize object
+shapex_object = ShapEx(timestep=0.0001,
+                       word_sampler=WordSamplerMode.BOLTZMANN, target_word_length=6, init_point=InitPoint.PSO,
+                       dir_sampling=DirectionSampling.CDHR, shrinking=Shrinking.SHRINKING,
+                       noise_dist='uniform', noise=0)
+
+
+# loads the spec
+shapex_object.add_shape_expression(path)
+
+# sample
+samples = shapex_object.samples(5)
+
+print(samples)
+
+plotter(samples)
