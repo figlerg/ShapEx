9f5a784d0e7cfafcca3a02bf08e61ceabacd3096
Felix Gigler <figlergelix@gmail.com>
2021-07-26 13:15:07 +0200

    updated setup file for pip

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import find_packages, setup
 
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ShapEx',
-    version='2021.5',
+    version='1.0.0',
     description='Library for sampling shape expressions.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/figlerg/ShapEx.git',
     license='BSD',
     author='Felix Gigler, Dejan Nickovic, Mateis Cristinel, Nicolas Basset, Thao Dang',
     author_email='felix.gigler.fl@ait.ac.at',
     python_requires='>=3.5',
-    install_requires=['tqdm', 'matplotlib'
+    install_requires=['tqdm', 'matplotlib','numpy', 'z3-solver', 'antlr4-python3-runtime','anyHR',
     ],
     packages=find_packages(),
 
     classifiers=[
         'License :: OSI Approved :: BSD License',
-	    'Programming Language :: Python :: 3.5',
-	    'Programming Language :: Python :: 3.6',
-	    'Programming Language :: Python :: 3.7',
+	    'Programming Language :: Python :: 3',
+        "Operating System :: OS Independent",
     ],
 )
